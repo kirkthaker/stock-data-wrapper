493a97048a408afffeae724dcbac3ccfb13932f3
kirkthaker <kirkthaker66@gmail.com>
2015-12-02 03:06:41 -0500

    completed get_all_data function

--- stockdata.py
+++ stockdata.py
@@ -1,4 +1,7 @@
 import urllib
+from collections import namedtuple
+
+Data = namedtuple("Data", "dates closes highs lows opens volumes")
 
 class StockData:
 
@@ -15,10 +18,42 @@ class StockData:
         Returns a Data object that contains the:
         date, close, high, low, open, and volume for the current stock
         over the specified period and number of intervals (each is an array).
-        Period is a string (i.e. '1d') and interval is an int (i.e. 60)
-        See readme.md for examples of usage and more detailed explanation.
+        Interval is an int (i.e. 60) and period is a string (i.e. '1d')
+        See readme.md for examples of usage and a more detailed explanation.
         """
+
+        dates = []
+        close_prices = []
+        high_prices = []
+        low_prices = []
+        open_prices = []
+        volumes = []
 
         params = {"q":self.symbol, "i":str(interval), "p":period, "f":"d,c,h,l,o,v"}
         encoded_params = urllib.urlencode(params)
         api_url = '%s%s' % (self.BASE_URL, encoded_params)
+
+        opened = urllib.urlopen(api_url)
+        raw_data = opened.read()
+
+        # split the data into its rows
+        # only need data from the 7th row onwards
+        split_data = raw_data.split()[8:]
+
+        for row in split_data:
+            row = row.split(",")
+            dates.append(int(row[0]))
+            close_prices.append(float(row[1]))
+            high_prices.append(float(row[2]))
+            low_prices.append(float(row[3]))
+            open_prices.append(float(row[4]))
+            volumes.append(int(row[5]))
+
+        return Data(
+            dates = dates,
+            closes = close_prices,
+            highs = high_prices,
+            lows = low_prices,
+            opens = open_prices,
+            volumes = volumes
+        )
