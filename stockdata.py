fb0b38bfc42c78ee0ef92919ed4b1978654209e9
kirkthaker <kirkthaker66@gmail.com>
2015-12-02 02:34:57 -0500

    url encode parameters

--- stockdata.py
+++ stockdata.py
@@ -1,3 +1,24 @@
+import urllib
+
 class StockData:
+
     BASE_URL = "http://www.google.com/finance/getprices?"
-    
+
+    def __init__(self, symbol):
+        """
+        Initialize with a single security, given its symbol
+        """
+        self.symbol = symbol
+
+    def get_all_data(self, interval, period):
+        """
+        Returns a Data object that contains the:
+        date, close, high, low, open, and volume for the current stock
+        over the specified period and number of intervals (each is an array).
+        Period is a string (i.e. '1d') and interval is an int (i.e. 60)
+        See readme.md for examples of usage and more detailed explanation.
+        """
+
+        params = {"q":self.symbol, "i":str(interval), "p":period, "f":"d,c,h,l,o,v"}
+        encoded_params = urllib.urlencode(params)
+        api_url = '%s%s' % (self.BASE_URL, encoded_params)
